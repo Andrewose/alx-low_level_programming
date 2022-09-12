cfd2e7fc02f52b4f87fb44cfc9d216e1b63f74d8
Andrew okonofua <andrewokonofua71@gmail.com>
2022-09-12 11:02:35 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
- * Return: Always 0 
+ * Return: Always 0 (success)
  */
-int main(void) 
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzel\n");
-	return(0);
+	puts("\"Programming is like building a mulitlingual puzzle\n");
+		return(0);
 }
