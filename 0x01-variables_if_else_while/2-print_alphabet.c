387d88d8a9afe77158f5e02242f3157ec512f264
Andrew okonofua <andrewokonofua71@gmail.com>
2022-09-12 12:54:22 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c + 1);
+	}
+	putchar('\n');
+	return (0);
+}
