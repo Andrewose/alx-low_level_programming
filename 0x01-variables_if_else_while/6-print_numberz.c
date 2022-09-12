588e6855989dbb3b5c222a6b85ead2b20beda5a0
Andrew okonofua <andrewokonofua71@gmail.com>
2022-09-12 14:56:13 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: ALways 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for ( c = '0'; c <= 9; c++ )
+	{
+		putchar(c);
+	}
+	putchar ('\n');
+		return (0);
+}
