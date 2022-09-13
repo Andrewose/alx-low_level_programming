eb9ee26f0fd357b9e253e1bdd76ba0200a22059f
Andrew okonofua <andrewokonofua71@gmail.com>
2022-09-13 16:09:33 -0700

    print last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ *
+ *  * print_last_digit - prints the last digit of the integer passed
+ *
+ *   * @n: the integer whose last digit to print
+ *
+ *    *
+ *
+ *     * Return: positive integer value of the last digit
+ *
+ *      */
+
+int print_last_digit(int n)
+
+{
+
+		int d = _abs(n % 10);
+
+
+
+			_putchar(d + '0');
+
+				return (d);
+
+}
+/**
+ *
+ *  * _abs - returns the absolute value of integer
+ *
+ *   * @n: the integer to value absolutely
+ *
+ *    *
+ *
+ *     * Return: a positive integer or zero
+ *
+ *      */
+
+int _abs(int n)
+
+{
+
+		return (n > 0 ? n : -n);
+
+}
