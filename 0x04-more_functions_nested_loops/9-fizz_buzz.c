c207047e7b0c4d43347eb1d88cc285cab9b8b12b
Andrew okonofua <andrewokonofua71@gmail.com>
2022-09-16 15:02:45 -0700

    print fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = 1; i < 100; i++)
 	{
-		if (!(i % 3))
+		if (!(i % 3 == 0))
 			printf("Fizz ");
-		else if (!(i % 5))
+		else if (!(i % 5 == 0))
 			printf("Buzz ");
-		else if (!(i % 3 && 5))
+		else if (!(i % 15 == 0))
 			printf("fizzBuzz ");
 		else
 			printf("%d ", i);
